018001f1689225248e27a86bf7d596769ab32cd0
Rolopens <rolopens@gmail.com>
2018-12-07 18:32:50 +0800

    Dialog pops up when attempting to open cashier and chef view (password: admin123)

--- app/src/main/java/com/example/rolo/ramennagikiosk/MainActivity.java
+++ app/src/main/java/com/example/rolo/ramennagikiosk/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.rolo.ramennagikiosk;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         ChefButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, chef_view.class);
-//                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
+                promptDialog("CHEF");
             }
         });
         CustomerButton.setOnClickListener(new View.OnClickListener() {
@@ -35,11 +38,53 @@ public class MainActivity extends AppCompatActivity {
         });
         CashierButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, cashier_view.class);
-                startActivity(intent);
+                promptDialog("CASHIER");
             }
         });
     }
+
+    public void promptDialog(String value){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.prompt);
+        final EditText userInput = new EditText(this);
+        builder.setView(userInput);
+        final String type = value;
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                String value;
+                Log.d("INPUT TAG", "INPUT MSG: " + userInput.getText().toString());
+                value = userInput.getText().toString();
+                if (type.equals("CHEF")){
+                    if (value.equals("admin123")){
+                        Intent intent = new Intent(MainActivity.this, chef_view.class);
+//                intent.putExtra(EXTRA_MESSAGE, message);
+                        startActivity(intent);
+                    } else {
+                        Log.d("INPUT TAG", "CHEF FAIL");
+                        Toast toast = Toast.makeText(getApplicationContext(), R.string.fail, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+
+                } else if (type.equals("CASHIER")){
+                    if (value.equals("admin123")){
+                        Intent intent = new Intent(MainActivity.this, cashier_view.class);
+                        startActivity(intent);
+                    } else {
+                        Log.d("INPUT TAG", "CASHIER FAIL");
+                        Toast toast = Toast.makeText(getApplicationContext(), R.string.fail, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+
+                }
+
+//
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 
 
 }
