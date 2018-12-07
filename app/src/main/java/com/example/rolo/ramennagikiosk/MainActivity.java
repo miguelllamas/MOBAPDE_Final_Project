90bc7bc8ad502a0884e4b26d74e498b6ab7a089f
Rolopens <rolopens@gmail.com>
2018-12-07 17:21:01 +0800

    Can move from one activity to another

--- app/src/main/java/com/example/rolo/ramennagikiosk/MainActivity.java
+++ app/src/main/java/com/example/rolo/ramennagikiosk/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.rolo.ramennagikiosk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button ChefButton;
+    Button CustomerButton;
+    Button CashierButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ChefButton = findViewById(R.id.chefButon);
+        CustomerButton = findViewById(R.id.custButton);
+        CashierButton = findViewById(R.id.cashButton);
+
+        ChefButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, chef_view.class);
+//                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
+        CustomerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, customer_view.class);
+                startActivity(intent);
+            }
+        });
+        CashierButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, cashier_view.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
