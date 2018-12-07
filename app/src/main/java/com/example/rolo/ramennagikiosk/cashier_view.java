a201fe1b2e05f2bf642fbbc70c11c7423e3e48b8
Rolopens <rolopens@gmail.com>
2018-12-07 16:31:12 +0800

    Created GUI for landing page - made empty activites for chef, customer, and cashier

---
+++ app/src/main/java/com/example/rolo/ramennagikiosk/cashier_view.java
@@ -0,0 +1,13 @@
+package com.example.rolo.ramennagikiosk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cashier_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cashier_view);
+    }
+}
